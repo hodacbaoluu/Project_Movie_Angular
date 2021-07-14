234a8b49fdc02035aa01e6b72d640bff7e912217
Hodacbaoluu <luuhodacbao@gmail.com>
2021-07-14 17:47:48 +0700

    sửa lại header trong admin, chưa render được listMovie

--- src/app/admin/movie-manager/movie-manager.component.ts
+++ src/app/admin/movie-manager/movie-manager.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList } from '@angular/core';
 import { MovieService } from 'src/app/pages/core/service/movie/movie.service';
 
 @Component({
   selector: 'app-movie-manager',
   templateUrl: './movie-manager.component.html',
-  styleUrls: ['./movie-manager.component.scss']
+  styleUrls: ['./movie-manager.component.scss'],
 })
 export class MovieManagerComponent implements OnInit {
-
   maNhom: number = 1;
+  soTrang: number = 1;
+
+  changePage(soTrang: any) {
+    this.soTrang = soTrang;
+    console.log(this.soTrang);
+  }
   constructor(private movieService: MovieService) {}
 
   movieList: any = [];
-
+  dataMovie: any = Object.values(this.movieList)
   getListMovie() {
-    this.movieService.getListMovieApi(this.maNhom).subscribe((data) => {
-      console.log(data);
+    this.movieService.getPaging(this.soTrang).subscribe((data) => {
+      
       this.movieList = data;
+      console.log(this.movieList.items)
+      console.log(this.dataMovie)
     });
   }
 
--- src/app/pages/core/service/movie/movie.service.ts
+++ src/app/pages/core/service/movie/movie.service.ts
@@ -1,46 +1,64 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import {tap,catchError} from 'rxjs/operators'
+import { HttpClient } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
-
-  constructor(private httpClient:HttpClient) { }
-  getListMovieApi(maNhom:number){
-    return this.httpClient.get(`https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP0${maNhom}`).pipe(
-      tap((data)=>{
-        console.log(maNhom);
-      }),
-      catchError((err)=>{
-        console.log(err);
-        return err;
-
-      })
-    );
-  }
-  getCarouselMovieApi(maPhim:number){
-    return this.httpClient.get(`https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayThongTinPhim?MaPhim=${maPhim}`).pipe(
-      tap((data)=>{
-
-
-      }),
-      catchError((err)=>{
-        console.log(err);
-        return err;
-
-      })
-    )
-  }
-  getListUserApi(){
-    return this.httpClient.get(`https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/LayDanhSachNguoiDung?MaNhom=GP10`).pipe(
-      tap((data)=>{
-
-      }),
-      catchError((err)=>{
-        return err
-      })
+  constructor(private httpClient: HttpClient) {}
+  getListMovieApi(maNhom: number) {
+    return this.httpClient
+      .get(
+        `https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP0${maNhom}`
       )
+      .pipe(
+        tap((data) => {
+          console.log(maNhom);
+        }),
+        catchError((err) => {
+          console.log(err);
+          return err;
+        })
+      );
+  }
+  getCarouselMovieApi(maPhim: number) {
+    return this.httpClient
+      .get(
+        `https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayThongTinPhim?MaPhim=${maPhim}`
+      )
+      .pipe(
+        tap((data) => {}),
+        catchError((err) => {
+          console.log(err);
+          return err;
+        })
+      );
+  }
+  getListUserApi() {
+    return this.httpClient
+      .get(
+        `https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/LayDanhSachNguoiDung?MaNhom=GP10`
+      )
+      .pipe(
+        tap((data) => {}),
+        catchError((err) => {
+          return err;
+        })
+      );
+  }
+  getPaging(soTrang: number) {
+    return this.httpClient
+      .get(
+        `https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhimPhanTrang?maNhom=GP01&soTrang=${soTrang}&soPhanTuTrenTrang=10`
+      )
+      .pipe(
+        tap((data) => {
+          console.log(data);
+        }),
+        catchError((err) => {
+          return err;
+        })
+      );
   }
 }
