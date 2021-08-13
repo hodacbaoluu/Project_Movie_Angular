f2b3ea84605068bf5930b930317f46bfbf32fe28
CongTan2661 <congtan3081@gmail>
2021-08-13 20:35:22 +0700

    responsive xong, chua sua modal video, loi tum lum :)))

--- src/app/pages/home/phim-dang-chieu/phim-dang-chieu.component.ts
+++ src/app/pages/home/phim-dang-chieu/phim-dang-chieu.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { MovieService } from '../../core/service/movie/movie.service';
-
+declare var $: any;
 @Component({
   selector: 'app-phim-dang-chieu',
   templateUrl: './phim-dang-chieu.component.html',
@@ -11,13 +11,76 @@ export class PhimDangChieuComponent implements OnInit {
   constructor(private phimDangChieuService: MovieService) { }
   danhSachPhimDangChieu: any = [];
   maNhom: number = 6;
+  getTrailer(phim: any) {
+    const index = this.danhSachPhimDangChieu.findIndex((item: any) => { return item.maPhim = phim.maPhim });
+    if (index !== -1) {
+      this.danhSachPhimDangChieu[index].trailer = phim.trailer;
+    }
+  }
   getNameMovie() {
     this.phimDangChieuService.getListMovieApi(this.maNhom).subscribe((data) => {
       this.danhSachPhimDangChieu = data;
     })
   }
+
   ngOnInit(): void {
     this.getNameMovie();
+
+
+
+    setTimeout(() => {
+      $('.lazy').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        rows: 2,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              rows: 2,
+              slidesToShow: 3,
+              slidesToScroll: 3,
+
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              rows: 1,
+              slidesToShow: 4,
+              slidesToScroll: 3,
+
+            }
+          },
+
+          {
+            breakpoint: 768,
+            settings: {
+
+              rows: 1,
+              slidesToShow: 4,
+              slidesToScroll: 3,
+            }
+
+          },
+          {
+            breakpoint: 560,
+            settings: {
+              rows: 1,
+              arrows: false,
+              slidesToShow: 5,
+              slidesToScroll: 4,
+
+            }
+          },
+        ]
+      });
+
+    }, 2000)
   }
+
+
+
 
 }
--- src/app/pages/home/phim-sap-chieu/phim-sap-chieu.component.ts
+++ src/app/pages/home/phim-sap-chieu/phim-sap-chieu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../core/service/movie/movie.service';
-
+declare var $: any;
 @Component({
   selector: 'app-phim-sap-chieu',
   templateUrl: './phim-sap-chieu.component.html',
@@ -11,7 +11,7 @@ export class PhimSapChieuComponent implements OnInit {
   constructor(private phimSapChieuService: MovieService) { }
   danhSachPhimSapChieu: any = [];
   danhSachLichChieu: any = [];
-  maNhom: number = 6;
+  maNhom: number = 7;
 
   getNameMovie() {
     this.phimSapChieuService.getListMovieApi(this.maNhom).subscribe((data) => {
@@ -29,6 +29,56 @@ export class PhimSapChieuComponent implements OnInit {
   // }
   ngOnInit(): void {
     this.getNameMovie();
+    setTimeout(() => {
+      $('.lazy').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        rows: 2,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              rows: 2,
+              slidesToShow: 3,
+              slidesToScroll: 3,
+
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              rows: 1,
+              slidesToShow: 4,
+              slidesToScroll: 3,
+
+            }
+          },
+
+          {
+            breakpoint: 768,
+            settings: {
+
+              rows: 1,
+              slidesToShow: 4,
+              slidesToScroll: 3,
+            }
+
+          },
+          {
+            breakpoint: 560,
+            settings: {
+              rows: 1,
+              arrows: false,
+              slidesToShow: 5,
+              slidesToScroll: 4,
+
+            }
+          },
+        ]
+      });
+
+    }, 2000)
   }
 
 
